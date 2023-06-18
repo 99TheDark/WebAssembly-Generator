e8b6e916877367bafd02e6c2b2ff721c512ca94c
99TheDark <99TheDark@gmail.com>
2023-06-17 20:45:40 -0700

    Trying to get NPM installation to work

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { WebAssemblyGenerator } from "./generator";
+import { WebAssemblyType, WebAssemblyIntegerType, WebAssemblyFloatingType } from "./types";
+
+export {
+    WebAssemblyGenerator,
+    WebAssemblyType,
+    WebAssemblyIntegerType,
+    WebAssemblyFloatingType
+};
